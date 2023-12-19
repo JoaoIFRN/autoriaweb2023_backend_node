bf245203f6713de4986d1a642662b4e67a75c11d
JoaoNascimento <joaoifrn@gmail.com>
2023-12-19 08:46:07 -0300

    Refatoração de código e ajustes

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const tarefasRouter = require('./controllers/TarefaController');
 
+// Cria uma instância do servidor Express.
+const app = express();
+
+// Aplica o middleware para parsear JSON no corpo das requisições.
 app.use(express.json());
+
+// Habilita o CORS para permitir requisições de diferentes origens.
 app.use(cors());
 
-app.use("/tarefas",tarefasRouter);
+// Define a rota "/tarefas" e associa ao router importado.
+app.use("/tarefas", tarefasRouter);
 
+// Define a porta do servidor, com um fallback para a porta 3000 se não estiver definida.
 const PORT = process.env.PORT || 3000;
 
+// Inicia o servidor na porta especificada.
 app.listen(PORT, () => {
   console.log(`Servidor Express rodando na porta ${PORT}`);
 });
--- controllers/TarefaController.js
+++ controllers/TarefaController.js
@@ -1,64 +1,51 @@
-// tasks.js
 const express = require('express');
 const router = express.Router();
 const db = require('../util/db');
 
+/**
+ * Executa uma consulta no banco de dados e envia uma resposta.
+ * @param {string} sql - A consulta SQL a ser executada.
+ * @param {Array} params - Os parâmetros para a consulta SQL.
+ * @param {Object} res - O objeto de resposta do Express.
+ * @param {string} erroMsg - Mensagem de erro para ser enviada em caso de falha.
+ */
+function executarConsulta(sql, params, res, erroMsg) {
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(500).json({ erro: erroMsg, detalhes: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+}
+
 // Rota para buscar todas as tarefas
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM tarefa', (err, result) => {
-    if (err) 
-      res.status(500).json({erro : "Erro na consulta de tarefas", error: err});
-    else
-      res.status(200).json(result);
-  });
+  executarConsulta('SELECT * FROM tarefa', [], res, "Erro na consulta de tarefas");
 });
 
-// Rota para buscar uma tarefa
+// Rota para buscar uma tarefa específica
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  db.query('SELECT * FROM tarefa where id = ?', [id], (err, result) => {
-    if (err) 
-      res.status(500).json({erro : "Erro na consulta de tarefa", error: err});
-    else
-      res.status(200).json(result);
-  });
+  executarConsulta('SELECT * FROM tarefa WHERE id = ?', [id], res, "Erro na consulta de tarefa");
 });
 
 // Rota para criar uma nova tarefa
 router.post('/', (req, res) => {
   const { titulo, descricao } = req.body;
-  db.query('INSERT INTO tarefa (titulo, descricao) VALUES (?, ?)', [titulo, descricao], (err, result) => {
-    if (err) { 
-      res.status(500).json({erro : "Erro no cadastro de tarefa!", error: err});
-    } else {
-      res.status(200).json({mensagem : 'Tarefa criada com sucesso!'});
-    }
-  });
+  executarConsulta('INSERT INTO tarefa (titulo, descricao) VALUES (?, ?)', [titulo, descricao], res, "Erro no cadastro de tarefa!");
 });
 
+// Rota para deletar uma tarefa
 router.delete("/:id", (req, res) => {
   const tarefaId = req.params.id;
-  const sql = 'DELETE FROM tarefa WHERE id = ?';;
-
-  db.query(sql, [tarefaId], (err, result) => {
-    if (err) {      
-      res.status(500).json({erro : 'Erro interno do servidor', error : err});
-    } else {
-      res.status(200).json({mensagem: 'Registro deletado com sucesso'});
-    }
-  });
+  executarConsulta('DELETE FROM tarefa WHERE id = ?', [tarefaId], res, 'Erro ao deletar tarefa');
 });
 
-// Rota para criar uma nova tarefa
+// Rota para atualizar uma tarefa
 router.put('/', (req, res) => {
   const { id, titulo, descricao } = req.body;
-  db.query('update tarefa set titulo = ?, descricao = ? where id = ?', [titulo, descricao, id], (err, result) => {
-    if (err) { 
-      res.status(500).json({erro : "Erro no cadastro de tarefa!", error: err});
-    } else {
-      res.status(200).json({mensagem : 'Tarefa criada com sucesso!'});
-    }
-  });
+  executarConsulta('UPDATE tarefa SET titulo = ?, descricao = ? WHERE id = ?', [titulo, descricao, id], res, "Erro ao atualizar tarefa");
 });
 
 module.exports = router;
